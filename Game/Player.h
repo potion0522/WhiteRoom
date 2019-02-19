120d1650f699175fdcd84896278c540ad859e1b7
tika <kt.potion0522w@gmail.com>
2019-02-19 23:28:57 +0900

    コメントの追加

--- Game/Player.h
+++ Game/Player.h
@@ -23,9 +23,9 @@ private:
 	const double _HEIGHT;
 
 private:
-	Vector _stand_pos;
-	Vector _head_pos;
-	Vector _past_pos;
+	Vector _stand_pos; // 計算用(足元)
+	Vector _head_pos;  // カメラ用
+	Vector _past_pos;  // 前回座標(足元)
 	Vector _dir;
 	unsigned char _floor;
 };
